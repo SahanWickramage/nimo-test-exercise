0cd7ab55f9cff3f8ed502da42afef94a3d179dd3
Sahan Wickramage <35292360+SahanWickramage@users.noreply.github.com>
2024-07-12 10:35:40 +1000

    fixed functional issues

--- cryptocurrency-price-service/app.mjs
+++ cryptocurrency-price-service/app.mjs
@@ -1,8 +1,10 @@
 import { DynamoDBClient, PutItemCommand } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocumentClient, ScanCommand } from "@aws-sdk/lib-dynamodb";
+import { SESClient, SendEmailCommand } from '@aws-sdk/client-ses';
 
 const ddbClient = new DynamoDBClient();
 const docClient = DynamoDBDocumentClient.from(ddbClient);
+const sesClient = new SESClient();
 
 const baseUrl = process.env.COINGECKO_URL;
 const apiKey = process.env.COINGECKO_API_KEY;
@@ -64,23 +66,39 @@ export const lambdaHandler = async (event, context) => {
 
         const headers = {
           'accept': 'application/json',
-          'x-cg-pro-api-key': apiKey
+          'x-cg-pro-api-key': 'CG-Puzd4QbkcqBh2aZkmSEoZe7J'
         };
+        console.log(`x-cg-pro-api-key: ${apiKey}`);
       
         const requestOptions = {
           method: 'GET',
           headers: headers
         };
 
-        const response = await fetch(`${baseUrl}/simple/price?ids=${coinId}&vs_currencies=usd`, requestOptions);
+        const url = `${baseUrl}/simple/price?ids=${coinId}&vs_currencies=usd`;
+        console.log(url);
+        const response = await fetch(url, requestOptions);
 
         if (!response.ok) {
           throw new Error(`HTTP error ${response.status}`);
         }
     
         const data = await response.json();
-        const cryptocurrencyPrice = data.coinId.usd;
+        
+        const jsonStr = JSON.stringify(data);
+        const jsonMap = new Map(Object.entries(JSON.parse(jsonStr)));
+        const usdObj = jsonMap.get("ethereum");
+        const cryptocurrencyPrice = usdObj.usd;
+        
+        console.log(`${cryptocurrencyPrice} type: ${typeof cryptocurrencyPrice}`);
+        
         const timeStamp = new Date().toISOString();
+
+        console.log(`id: ${Date.now().toString()} | type: ${typeof Date.now().toString()}`);
+        console.log(`coinId: ${coinId} | type: ${typeof coinId}`);
+        console.log(`price: ${cryptocurrencyPrice} | type: ${typeof cryptocurrencyPrice}`);
+        console.log(`timeStamp: ${timeStamp} | type: ${typeof timeStamp}`);
+        console.log(`requestedBy: ${email} | type: ${typeof email}`);
 
         const putItemParams = {
           TableName: dynamodbTableName,
@@ -88,7 +106,7 @@ export const lambdaHandler = async (event, context) => {
             // The key-value pairs representing the item to be added
             "id": { S: Date.now().toString() }, // Partition key
             "coinId": { S: coinId },
-            "price": { N: cryptocurrencyPrice },
+            "price": { N: cryptocurrencyPrice.toString() },
             "timeStamp": { S:  timeStamp},
             "requestedBy": { S: email }
           }
@@ -98,10 +116,34 @@ export const lambdaHandler = async (event, context) => {
 
         const dynamodbResponse = await ddbClient.send(command);
         console.log("Item added successfully:", dynamodbResponse);
+        
+        const emailParams = {
+          Source: sourceEmail,
+          Destination: {
+            ToAddresses: [email],
+          },
+          Message: {
+            Subject: {
+              Data: `Cryptocurrency price`,
+            },
+            Body: {
+              Text: {
+                Data: `Price of ${coinId} is ${cryptocurrencyPrice}!`,
+              },
+            },
+          },
+        };
+        
+        const sendEmailCommand = new SendEmailCommand(emailParams);
+        
+        const sesResponse = await sesClient.send(sendEmailCommand);
+        console.log('Email sent:', sesResponse);
 
         return {
           statusCode: 200,
-          body: JSON.stringify({"dynamodbResponse": dynamodbResponse})
+          body: JSON.stringify({
+            "message": "Email successfully sent!",
+            "dynamodbResponse": dynamodbResponse})
         };
       }
       }
@@ -110,9 +152,7 @@ export const lambdaHandler = async (event, context) => {
     console.error(err);
     return {
       statusCode: 500,
-      body: JSON.stringify({
-        message: 'An error occurred while processing your request.',
-      }),
+      body: JSON.stringify({ error: err.message })
     };
   } 
   
--- search-history-service/app.mjs
+++ search-history-service/app.mjs
@@ -2,36 +2,28 @@ import { LambdaClient, InvokeCommand } from "@aws-sdk/client-lambda";
 
 const client = new LambdaClient();
 
-/**
- *
- * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
- * @param {Object} event - API Gateway Lambda Proxy Input Format
- *
- * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html 
- * @param {Object} context
- *
- * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
- * @returns {Object} object - API Gateway Lambda Proxy Output Format
- * 
- */
+const functionName = process.env.LAMBDA_FUNCTION_NAME;
 
 export const lambdaHandler = async (event, context) => {
   
   const params = {
-        FunctionName: 'arn:aws:lambda:ap-southeast-2:211125780252:function:nimo-test-exercise-CryptocurrencyPriceFunction-Fgpj5YBI7vdK',
+        FunctionName: `arn:aws:lambda:ap-southeast-2:211125780252:function:${functionName}`,
         InvocationType: 'RequestResponse',
-        Payload: JSON.stringify({ name: 'John Doe' })
+        Payload: JSON.stringify({isLambdaRequest: true})
     };
     
     const lambdaResponse = await client.send(new InvokeCommand(params));
     console.log(lambdaResponse);
+    
+    const jsonString = new TextDecoder().decode(lambdaResponse.Payload);
+    console.log(jsonString);
+    
+    const jsonObject = JSON.parse(jsonString);
+    console.log(jsonObject);
   
     const response = {
       statusCode: 200,
-      body: JSON.stringify({
-        message: 'this is search history service!',
-        responseFromLambda: lambdaResponse
-      })
+      body: JSON.stringify(jsonObject)
     };
 
     return response;
