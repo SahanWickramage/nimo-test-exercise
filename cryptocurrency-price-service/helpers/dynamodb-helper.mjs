f2772ac19e2564979c3ea13eedf3782eb224c96e
Sahan Wickramage <35292360+SahanWickramage@users.noreply.github.com>
2024-07-20 20:14:28 +1000

    refactored price service code 3

--- cryptocurrency-price-service/helpers/dynamodb-helper.mjs
+++ cryptocurrency-price-service/helpers/dynamodb-helper.mjs
@@ -6,6 +6,7 @@ class DynamodbHelper {
     constructor() {
         this.dynamodbTableName = process.env.DYNAMODB_TABLE;
         this.dynamodbClient = new DynamoDBClient();
+        this.dynamodbDocClient = DynamoDBDocumentClient.from(dynamodbClient);
     }
 
     #getPutItemParams(cryptocurrencyId, cryptocurrencyPrice, requestedBy) {
@@ -33,6 +34,55 @@ class DynamodbHelper {
         } catch (err) {
             throw new Error(`Search query saving failed. ${err.message}`);
         }
+    }
+
+    #getScanParams() {
+        return {
+            TableName: dynamodbTableName
+        };
+    }
+
+    getHistory() {
+        try {
+            const fetchedItems = this.#getAllItems();
+            const queryHistoryMap = this.#groupItems(fetchedItems);
+            console.log("Fetching history succeed!");
+            return queryHistoryMap;
+        } catch (err) {
+            throw new Error(`Fetching history failed! ${err.message}`);
+        }    
+    }
+
+    async #getAllItems() {
+        try {
+            const scanParams = this.#getScanParams();
+
+            const scanResponse = await this.dynamodbDocClient.send(new ScanCommand(scanParams));
+
+            const fetchedItems = [...scanResponse.Items];
+            while (scanResponse.LastEvaluatedKey) {
+                scanParams.ExclusiveStartkey = scanResponse.LastEvaluatedKey;
+                scanResponse = await this.dynamodbDocClient.send(new ScanCommand(scanParams));
+                fetchedItems.push(...scanResponse.Items);
+            }
+
+            console.log("Fetching items succeed!");
+
+            return fetchedItems;
+        } catch (err) {
+            throw new Error(`Fetching items failed! ${err.message}`);
+        }
+    }
+
+    #groupItems(fetchedItems) {
+        return fetchedItems.reduce((acc, obj) => {
+            const coinId = obj.coinId;
+            if (!acc[coinId]) {
+              acc[coinId] = [];
+            }
+            acc[coinId].push(obj.timeStamp);
+            return acc;
+          }, {});
     }
 
 }
