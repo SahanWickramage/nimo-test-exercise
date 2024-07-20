8ec16caafcde229c46d7765aceac1a626aa2282e
Sahan Wickramage <35292360+SahanWickramage@users.noreply.github.com>
2024-07-20 22:09:04 +1000

    refactored history service code

--- search-history-service/app.mjs
+++ search-history-service/app.mjs
@@ -1,31 +1,27 @@
-import { LambdaClient, InvokeCommand } from "@aws-sdk/client-lambda";
+import LambdaHelper from "./helpers/lambda-helper.mjs";
 
-const client = new LambdaClient();
-
-const functionName = process.env.LAMBDA_FUNCTION_NAME;
+const lambdaHelper = new LambdaHelper();
 
 export const lambdaHandler = async (event, context) => {
   
-  const params = {
-        FunctionName: `arn:aws:lambda:ap-southeast-2:211125780252:function:${functionName}`,
-        InvocationType: 'RequestResponse',
-        Payload: JSON.stringify({isLambdaRequest: true})
-    };
-    
-    const lambdaResponse = await client.send(new InvokeCommand(params));
-    console.log(lambdaResponse);
-    
-    const jsonString = new TextDecoder().decode(lambdaResponse.Payload);
-    console.log(jsonString);
-    
-    const jsonObject = JSON.parse(jsonString);
-    console.log(jsonObject);
-  
-    const response = {
+  try {
+    const lambdaResponsePayload = lambdaHelper.invokeLambda();
+    return {
       statusCode: 200,
-      body: JSON.stringify(jsonObject)
+      body: JSON.stringify({
+        "message": "History fetching succeed!",
+        "payload": lambdaResponsePayload
+      })
     };
 
-    return response;
-  };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        "message": "History fetching failed!",
+        "payload": null
+      })
+    };
+  }
+};
   
